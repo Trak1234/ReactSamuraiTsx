7e211c533c2f19792e4e6bb613c5e54ebf4c38af
ArtemS <etogramm82@mail.ru>
2022-02-17 21:04:27 +0300

    test

--- my-app/src/Components/Header/Header.tsx
+++ my-app/src/Components/Header/Header.tsx
@@ -8,6 +8,7 @@ const Header = () => {
         <header className={s.header}>
             <img
                 src='https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2019/08/10-Best-Gaming-Team-Logos-and-How-to-Make-Your-Own-CurrentYear-image1-1.png'/>
+            afsfas
         </header>
     );
 };
