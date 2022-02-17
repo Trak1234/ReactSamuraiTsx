733d865a29e5db865ff8528f5c64ba14bbd67c6a
ArtemS <etogramm82@mail.ru>
2022-02-17 20:41:37 +0300

    samurai1

---
+++ my-app/src/Components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Header.module.css';
+import * as https from "https";
+
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+            <img
+                src='https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2019/08/10-Best-Gaming-Team-Logos-and-How-to-Make-Your-Own-CurrentYear-image1-1.png'/>
+        </header>
+    );
+};
+
+export default Header;
